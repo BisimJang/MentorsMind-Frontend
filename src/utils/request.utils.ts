4bb53108eb25b139ca82d9d870e934923ee1aa75
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-25 10:25:29 +0100

    clean up: maintains as a transport layer only

--- src/utils/request.utils.ts
+++ src/utils/request.utils.ts
@@ -1,30 +1,16 @@
 import type { AxiosRequestConfig } from "axios";
 import api from "../services/api.client";
-import { getCached, setCache } from "./cache.utils";
 
 export const request = async <T>(
   config: AxiosRequestConfig,
   opts?: {
     signal?: AbortSignal;
-    useCache?: boolean;
   },
 ): Promise<T> => {
-  const key = JSON.stringify(config);
-
   if (opts?.signal) {
     config.signal = opts.signal;
   }
 
-  if (opts?.useCache) {
-    const cached = getCached<T>(key);
-    if (cached) return cached;
-  }
-
   const res = await api(config);
-
-  if (opts?.useCache) {
-    setCache(key, res.data);
-  }
-
   return res.data;
 };
